816c95af4eba09b3a9df6bde7d7c59c7155d9515
Pierre Weill-Tessier <pierre.weill-tessier@kcl.ac.uk>
2026-01-27 09:26:23 +0000

    Replace old Vue.extend() to new defineComponent() + few basic fixes in Vue components

--- src/components/Frame.vue
+++ src/components/Frame.vue
@@ -93,7 +93,7 @@
 //////////////////////
 //      Imports     //
 //////////////////////
-import Vue from "vue";
+import Vue, { defineComponent } from "vue";
 import FrameHeader from "@/components/FrameHeader.vue";
 import LabelSlotsStructureComponent from "@/components/LabelSlotsStructure.vue";
 import CaretContainer from "@/components/CaretContainer.vue";
@@ -112,7 +112,7 @@ import {getDateTimeFormatted, isMacOSPlatform, removeIf} from "@/helpers/common"
 //////////////////////
 //     Component    //
 //////////////////////
-export default Vue.extend({
+export default defineComponent({
     name: "Frame",
 
     components: {
@@ -126,7 +126,7 @@ export default Vue.extend({
 
     props: {
         // NOTE that type declarations here start with a Capital Letter!!! (different to types.ts!)
-        frameId: Number, // Unique Indentifier for each Frame
+        frameId: {type: Number, required: true}, // Unique Indentifier for each Frame
         isDisabled: Boolean,
         isBeingDragged: Boolean,
         frameType: {
