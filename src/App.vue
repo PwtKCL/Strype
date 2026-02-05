6ad5a2b997ead125493a3bed811cc231a869d122
Pierre Weill-Tessier <pierre.weill-tessier@kcl.ac.uk>
2026-02-05 12:29:51 +0000

    EXTRA-- 0c0e478d239  forgotten uses of vueComponentAPIHandler

--- src/App.vue
+++ src/App.vue
@@ -149,7 +149,7 @@ import { eventBus, projectDocumentationFrameId } from "@/helpers/appContext";
 import {inflateRaw} from "pako";
 import { Base64 } from "js-base64";
 import { BvTriggerableEvent } from "bootstrap-vue-next";
-import { vueComponentsAPIHandler } from "./helpers/vueComponentAPI";
+import { vueComponentsAPIHandler } from "@/helpers/vueComponentAPI";
 
 let autoSaveTimerId = -1;
 let projectSaveFunctionsState : ProjectSaveFunction[] = [];
