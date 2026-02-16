24c4b774badd6cec6ac3d69a731a4e567498db48
Pierre Weill-Tessier <pierre.weill-tessier@kcl.ac.uk>
2026-02-16 14:17:11 +0000

    EXTRA-- 89a454528f92f bootstrap styling

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,7 @@
         </div>
         <!-- #v-endif-->
         <!-- Keep the style position of the row div to get proper z order layout of the app -->
+        <div class="row g-0" style="position: relative;">
             <!-- the container div is only here because the new version of Splitpanes doesn't get the classes -->
             <div class="strype-split-theme">
                 <Splitpanes @resize=onStrypeCommandsSplitPaneResize>
